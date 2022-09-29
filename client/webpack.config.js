e0f7430b63dea52e4af3789e1700f5926a43b9bc
Matt Neal <neal.matt.m@gmail.com>
2022-09-29 15:34:31 -0400

    updated webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'Text Editor',
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
