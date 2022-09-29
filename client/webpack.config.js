b49107061dca45cb5e3ac154c534a267264d962a
Matt Neal <neal.matt.m@gmail.com>
2022-09-29 15:25:34 -0400

    Added webpack plugins to generate HTML, a service worker, and a manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,30 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: 'Text Editor',
+        description: 'Edit text files with this text editor.',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ],
+      })
     ],
 
     module: {
