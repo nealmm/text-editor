47bc1f94933665333dda8232c09256afefa20ba9
Matt Neal <neal.matt.m@gmail.com>
2022-10-03 21:10:10 -0400

    :(

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,6 +8,9 @@ app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(require('./routes/htmlRoutes')(app));
+require('./routes/htmlRoutes')(app);
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/favicon.ico'));
+});
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
